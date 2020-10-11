69e63c4fc85b45b09f4ea3fda6d3e431872735d7
Geesong Han <j.gs.han@outlook.com>
2020-10-11 13:17:56 +1300

    Initial commit

---
+++ src/main/java/com/westpac/assessment/post/retriever/PostRetrieverApplication.java
@@ -0,0 +1,13 @@
+package com.westpac.assessment.post.retriever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostRetrieverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostRetrieverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/westpac/assessment/post/retriever/PostRetrieverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.westpac.assessment.post.retriever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostRetrieverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
